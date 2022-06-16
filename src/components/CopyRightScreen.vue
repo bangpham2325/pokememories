28377148bc9f78f645959676c93f1dca235b1132
Pham Huu Bang <bangpham2325@gmail.com>
2022-06-16 16:50:58 +0700

    poke ne

---
+++ src/components/CopyRightScreen.vue
@@ -0,0 +1,17 @@
+<template>
+    <p class="copyright"> This game owned by Bang Dep Trai</p>
+</template>
+<style>
+.copyright {
+  position: fixed;
+  left: 50%;
+  transform: translateX(-50%);
+  bottom: 1.5rem;
+  color: var(--light);
+  z-index: 3;
+  font-size: 1.5rem;
+}
+.copyright a {
+  color: #f4dc26;
+}
+</style>
